da220a55edd45cae9c02884a49ab913cf0261865
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-01 14:45:19 -0700

    update

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full grid grid-cols-12">
+      <div className="border-r border-neutral-700 col-span-2">
+        <p className="text-4xl">ofall.app</p>
+      </div>
+
+      <div>Love</div>
+    </div>
+  );
+}
+
+export default App;
