1467594bd16967c3cd9b21c3643df75886f8d5b2
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-01 14:50:05 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="h-full grid grid-cols-12">
-      <div className="border-r border-neutral-700 col-span-2">
-        <p className="text-4xl">ofall.app</p>
+      <div className="border-r space-y-2 border-neutral-700 col-span-2 p-4">
+        <h1 className="text-xl font-bold tracking-wider">OFALL.APP</h1>
+        <button className="text-lg">notes</button>
       </div>
 
-      <div>Love</div>
+      <div className="p-4">Love</div>
     </div>
   );
 }
