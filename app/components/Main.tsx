650164dfbe05d2aed35df4b691718ec6e00238af
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-19 14:14:55 -0700

    update

--- app/components/Main.tsx
+++
@@ -1,9 +0,0 @@
-export const Main = () => {
-  return (
-    <div className="col-span-10 py-5 pr-5">
-      <div className="relative border border-neutral-400 h-full">
-        <p className="absolute -top-3 left-2 bg-white px-1">ASCII art</p>
-      </div>
-    </div>
-  );
-};
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Love</h1>
+    </div>
+  );
+}
