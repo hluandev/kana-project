b803912629cf6829f7aa32d01a1c6025faf444bf
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-21 00:01:18 -0700

    update

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,16 @@
 export const Nav = () => {
   return (
-    <nav className="py-4 border-b border-b-neutral-400">
-      <div className="mx-auto max-w-4xl">Menu</div>
+    <nav className="py-3 border-b border-b-neutral-400">
+      <div className="mx-auto max-w-4xl flex justify-between items-center">
+        <div className="flex gap-10">
+          <div>LEARN</div>
+          <div>GLOBAL CHART</div>
+        </div>
+
+        <div>
+          <div className="h-8 w-8 bg-black rounded-full"></div>
+        </div>
+      </div>
     </nav>
   );
 };
