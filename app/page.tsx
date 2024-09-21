6a730612dbf455cb9779bc5bcc6e7043a225ffd3
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-20 23:56:53 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Nav } from "@/components/Nav";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Love is here</h1>
+    <div className="h-full">
+      <Nav />
+
+      <div className="max-w-4xl mx-auto">
+        <h1>Love is here</h1>
+      </div>
     </div>
   );
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,7 @@
+export const Nav = () => {
+  return (
+    <nav className="py-4 border-b border-b-neutral-400">
+      <div className="mx-auto max-w-4xl">Menu</div>
+    </nav>
+  );
+};
