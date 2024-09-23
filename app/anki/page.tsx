e4f06c2dc49369e60be8be0e52f66648e27883f7
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-22 23:43:22 -0700

    update

--- app/anki/page.tsx
+++ app/anki/page.tsx
@@ -1,31 +1,28 @@
 const Anki = () => {
   return (
     <div className="relative h-full">
-      <div className="flex justify-between">
-        <div className="space-y-0.5">
+      <div className="relative flex justify-between">
+        <div className="">
           <p className="font-semibold text-lg">TheLoong</p>
           <div className=" bg-gradient-to-t w-72 font-semibold from-[#df1818] to-red-800 border border-yellow-300  rounded-sm text-xl text-white text-center py-1">
             100
           </div>
         </div>
 
-        <div className="text-6xl font-semibold leading-none">10</div>
+        <div className="text-6xl absolute left-1/2 -translate-x-1/2 font-semibold leading-none">
+          10
+        </div>
 
-        <div className="flex gap-2">
-          <div>
-            <p className="font-semibold text-lg text-right">幽玄獣 (Yūgenjū)</p>
-            <div className=" bg-gradient-to-t w-72 font-semibold from-purple-600 to-purple-900 border border-yellow-300  rounded-sm text-xl text-white text-center py-1">
-              100
-            </div>
-          </div>
-          <div className="flex justify-end">
-            <img src="/img/book.webp" className="h-16  rounded-lg" alt="" />
+        <div className="">
+          <p className="font-semibold text-lg text-right">幽玄獣 (Yūgenjū)</p>
+          <div className=" bg-gradient-to-t w-72 font-semibold from-purple-600 to-purple-900 border border-yellow-300  rounded-sm text-xl text-white text-center py-1">
+            20
           </div>
         </div>
       </div>
 
       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-        <p className="text-6xl font-medium text-center">幽玄獣</p>
+        <p className="text-6xl font-medium text-center">声</p>
         <img src="/img/book.webp" className="h-96  rounded-3xl mt-10" alt="" />
       </div>
 
