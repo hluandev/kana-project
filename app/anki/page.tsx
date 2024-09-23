ab3398b5182f079df234dc2090c1b84a0d6cf159
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-22 22:52:10 -0700

    update

--- app/anki/page.tsx
+++ app/anki/page.tsx
@@ -1,5 +1,30 @@
+import { Swords, ListIcon, Diamond, ListCheck, Sword } from "lucide-react";
+
 const Anki = () => {
-  return <div></div>;
+  return (
+    <div className="relative h-full ">
+      <div className="absolute space-y-10 left-1/2 -translate-x-1/2 top-1/3 -translate-y-1/2">
+        <p className="text-6xl font-medium text-center">本</p>
+        <img src="/img/book.webp" className="h-96  rounded-3xl" alt="" />
+      </div>
+
+      <form
+        action=""
+        className=" left-1/2 -translate-x-1/2 absolute  bottom-20 space-x-2"
+      >
+        <input
+          type="text"
+          placeholder="Attack"
+          className="bg-[#f6f6f6] w-96 text-center  shadow-inner outline-none rounded-lg border border-neutral-300 py-3 px-4"
+        />
+        <input
+          value={"Fight "}
+          type="submit"
+          className="bg-blue-500 text-white font-semibold p-3 w-24 rounded-md"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Anki;
--- components/Screen.tsx
+++ components/Screen.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 export const Screen = ({ children }: Props) => {
   return (
-    <div className="col-span-10">
+    <div className="col-span-10 h-full">
       <div className="border-b border-neutral-300 py-4 px-5 font-semibold bg-[#f3f3f3]">
         Anki
       </div>
-      <div className="p-5">{children}</div>
+      <div className="p-5 h-full">{children}</div>
     </div>
   );
 };
