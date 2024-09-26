daa2802b5e1fdedf87818a06143424b2a6fb11d5
Luan <97277916+hluandev@users.noreply.github.com>
2024-09-25 21:57:33 -0700

    update

--- actions/setLastNumber.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { cookies } from "next/headers";
-
-export async function setLastNumber(id: any) {
-  cookies().set("lastNumber", id.toString());
-}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="h-full ">This is tasks</div>;
-}
