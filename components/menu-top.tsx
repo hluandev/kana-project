ae182d4d55402c56d147d72935e53a7bee36919e
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 00:45:18 -0800

    update

--- components/menu-top.tsx
+++ components/menu-top.tsx
@@ -1,7 +1,7 @@
 export const MenuTop = () => {
   return (
     <div className="flex justify-between p-12">
-      <div className="text-2xl font-semibold">Shogun</div>
+      <div className="text-2xl font-semibold">KamiKana</div>
 
       <div className="p-3 bg-white/20">Player</div>
     </div>
