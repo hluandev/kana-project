6f7de69a2c18a202509b699ccfd0a2ed695bd76a
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 00:52:21 -0800

    update

--- components/menu-options.tsx
+++ components/menu-options.tsx
@@ -2,11 +2,11 @@ import { MenuLink } from "./menu-link";
 
 export const MenuOptions = () => {
   return (
-    <div className="fixed top-1/2 -translate-y-1/2 ml-12 flex flex-col gap-6 text-5xl font-bold">
+    <div className="fixed top-1/2 uppercase -translate-y-1/2 ml-12 flex flex-col gap-5 text-5xl font-bold">
       <MenuLink href="/play" text="Play" />
       <MenuLink href="/play" text="Leaderboard" />
       <MenuLink href="/play" text="Setting" />
-      <MenuLink href="/play" text="Socials" />
+      <MenuLink href="/play" text="Social" />
     </div>
   );
 };
