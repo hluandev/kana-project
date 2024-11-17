0e7f38b3e115ce8d1a01ba0ac13cbec7c92183b9
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 19:19:44 -0800

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,16 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className=" h-full bg-pink-900 flex justify-center items-center">
+      <form
+        action=""
+        className="flex flex-col gap-3 text-black p-4 bg-black rounded-md"
+      >
+        <input type="email" />
+        <input type="password" />
+        <input type="submit" className="text-white" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div className=""></div>;
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  } else {
+    redirect("/menu");
+  }
 }
