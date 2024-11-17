a6df36771e2e5e201143624f6f11f1f229bc0dfe
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 19:25:36 -0800

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,17 @@
+import { login } from "@/actions/auth";
+
 const Login = () => {
   return (
     <div className=" h-full bg-pink-900 flex justify-center items-center">
-      <form
-        action=""
-        className="flex flex-col gap-3 text-black p-4 bg-black rounded-md"
-      >
-        <input type="email" />
-        <input type="password" />
-        <input type="submit" className="text-white" />
+      <form className="flex flex-col gap-3 text-black p-4 bg-black rounded-md">
+        <input name="email" id="email" type="email" placeholder="Email" />
+        <input
+          name="password"
+          id="password"
+          type="password"
+          placeholder="Password"
+        />
+        <input formAction={login} type="submit" className="text-white" />
       </form>
     </div>
   );
