fa962b107dbdc4425e9d465edef4a05865b6fe0a
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 20:47:27 -0800

    update

--- components/enemy-card.tsx
+++ components/enemy-card.tsx
@@ -7,18 +7,25 @@ interface Props {
 }
 
 export const EnemyCard = ({ data }: Props) => {
+  const [id, setId] = useState(0);
   const [cards, setCards] = useState(data);
-
-  console.log(cards);
 
   return (
     <div className="flex flex-col gap-0.5 items-center h-fit">
       <div className="bg-red-600  border w-72 border-red-500 rounded-md p-2 text-center font-bold text-xl italic">
         20
       </div>
-      <div className="text-7xl  bg-black/70 backdrop-blur-2xl flex justify-center items-center h-72 aspect-square rounded-md">
-        {cards[0].japanese}
-      </div>
+      {cards.map(
+        (item: any, index: number) =>
+          index === id && (
+            <div
+              key={index}
+              className="text-7xl  bg-black/70 backdrop-blur-2xl flex justify-center items-center h-72 aspect-square rounded-md"
+            >
+              {item.japanese}
+            </div>
+          )
+      )}
     </div>
   );
 };
