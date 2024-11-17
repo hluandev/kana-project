13bc65f902157004efbdb2c8e26844b293476e0c
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 16:28:50 -0800

    update

--- components/menu-top.tsx
+++ components/menu-top.tsx
@@ -1,15 +1,13 @@
 export const MenuTop = () => {
   return (
     <div className="flex justify-between p-12">
-      <div className=" bg-red-600 p-3 rounded-lg">
-        <img src="/img/logo.svg" className="w-60" alt="" />
-      </div>
+      <img src="/img/logo.svg" className="w-60" alt="" />
 
       <div className="flex bg-white/20 backdrop-blur-xl  border">
         <div className="h-full aspect-square bg-white text-black flex justify-center text-xl font-bold items-center">
           13
         </div>
-        <div className="w-52 pl-3 uppercase font-semibold flex items-center text-lg">
+        <div className="w-60 pl-3 uppercase font-semibold flex items-center text-lg">
           Ruan
         </div>
       </div>
