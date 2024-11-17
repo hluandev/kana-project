0821c935a9e27d40ea101d7d839e8d1e4a5e675e
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 22:33:31 -0800

    update

--- data/play-mode.ts
+++ data/play-mode.ts
@@ -1,8 +1,17 @@
 export const PlayMode = [
-  { text: "Kana", desc: "Hiragana and Katakana for absolute beginner" },
+  {
+    text: "Kana",
+    desc: "Hiragana and Katakana for absolute beginner",
+    note: "Free",
+  },
   {
     text: "Solo",
     desc: "Take an adventure to conquer the basic vocabulary Japanese",
+    note: "Free for the first 50 words",
   },
-  { text: "Battle", desc: "Versus other players at who is better Japanese" },
+  {
+    text: "Battle",
+    desc: "Versus other players at who is better Japanese",
+    note: "Free to try 3 matches",
+  },
 ];
