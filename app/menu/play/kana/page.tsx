9c1bffded5da5a52b2dfd8cec01b9645607cf8cb
Luan <97277916+hluandev@users.noreply.github.com>
2024-11-16 17:49:20 -0800

    update

--- app/menu/play/kana/page.tsx
+++ app/menu/play/kana/page.tsx
@@ -1,21 +1,25 @@
+import { LockIcon } from "lucide-react";
+import Link from "next/link";
+
 const Kana = () => {
   return (
-    <div className="flex-1 z-10 flex justify-center items-center px-12">
+    <div className="fixed top-1/2 -translate-y-1/2 left-1/2 w-full -translate-x-1/2 z-10 flex justify-center items-center px-12">
       <div className="grid grid-cols-4 gap-1 font-mono ">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item, index) => (
-          <div
+          <Link
+            href={`kana/${item.toString()}`}
             className={`${
               item === 5 || item === 10 ? "bg-red-800/80" : "bg-neutral-800/80"
-            } hover:bg-pink-600 relative cursor-pointer text-3xl rounded-md overflow-hidden backdrop-blur-xl font-bold w-60 flex justify-center items-center aspect-video`}
+            } hover:bg-orange-600 relative cursor-pointer text-3xl rounded-md overflow-hidden backdrop-blur-xl font-bold w-60 flex justify-center items-center aspect-video`}
             key={index}
           >
             {item === 1 && (
-              <div className="text-sm absolute top-2 right-2 bg-orange-500/80 px-1.5 rounded-md">
+              <div className="text-xs absolute top-2 right-2 bg-orange-500/80 px-1.5 rounded-md">
                 Current
               </div>
             )}
-            <p>{item}</p>
-          </div>
+            <p>{item === 1 ? "あ" : <LockIcon strokeWidth={1.5} />}</p>
+          </Link>
         ))}
       </div>
     </div>
