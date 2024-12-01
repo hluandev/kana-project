e89e3c91ff18ed2e8ad385dd0fa0672d2c963ec5
Luan <97277916+hluandev@users.noreply.github.com>
2024-12-01 13:06:19 -0800

    update

--- app/menu/activity/page.tsx
+++ app/menu/activity/page.tsx
@@ -8,8 +8,8 @@ export default function Activity() {
   const { info } = usePlayerStore();
   return (
     <div className="h-full w-full pr-4">
-      <div className="flex flex-col bg-white/50 space-y-4 rounded-2xl w-full p-8 h-full">
-        <div className="space-y-2">
+      <div className="flex flex-col bg-white/50 space-y-4 rounded-2xl w-full p-4 h-full">
+        <div className="space-y-2 px-4 ">
           <h2 className="text-3xl font-semibold">Hello {info.first_name},</h2>
           <p className="text-2xl font-medium">Level {info.level}</p>
         </div>
@@ -17,7 +17,7 @@ export default function Activity() {
           <ActivityBox title="Matches" value={info.wins + info.losses} />
           <ActivityBox title="Wins" value={info.wins} />
           <ActivityBox title="Losses" value={info.losses} />
-          <ActivityBox title="Highest Score" value={info.highest_score} />
+          <ActivityBox title="Highest Hands" value={info.highest_score} />
         </div>
         <GraphTable />
       </div>
