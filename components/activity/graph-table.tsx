92c734a169cca807bd55e47118c17fde8aa1f98b
Luan <97277916+hluandev@users.noreply.github.com>
2024-12-01 13:19:50 -0800

    update

--- components/activity/graph-table.tsx
+++ components/activity/graph-table.tsx
@@ -1,3 +1,37 @@
+import { usePlayerStore } from "@/stores/usePlayerStore";
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
 export const GraphTable = () => {
-  return <div className="bg-white p-4 h-full rounded-2xl">GraphTable</div>;
+  const { activity } = usePlayerStore();
+
+  return (
+    <div className="bg-white  pt-10 pb-6 pr-10 h-full rounded-2xl">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={activity}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="created_at" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="wins"
+            stroke="#8884d8"
+            activeDot={{ r: 10 }}
+          />
+          <Line type="monotone" dataKey="losses" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
