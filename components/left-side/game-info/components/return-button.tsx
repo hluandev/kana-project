aa12940459f5276f0f892796c6bb280c9ba9d823
ddddddsssssggggg <144175324+ddddddsssssggggg@users.noreply.github.com>
2024-12-03 10:47:40 -0800

    update

--- components/left-side/game-info/components/return-button.tsx
+++ components/left-side/game-info/components/return-button.tsx
@@ -6,7 +6,7 @@ export const ReturnButton = () => {
     <Link
       prefetch={true}
       href={"/menu"}
-      className="w-12 h-12  rounded-md flex items-center justify-center"
+      className="pr-3 flex items-center justify-center"
     >
       <Undo2Icon className="w-4 h-4" />
     </Link>
