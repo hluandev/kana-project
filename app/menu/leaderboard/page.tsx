06c95bf452082373e7f2d48d544b0cb71b481301
ddddddsssssggggg <144175324+ddddddsssssggggg@users.noreply.github.com>
2024-12-05 14:15:19 -0800

    updatwe

--- app/menu/leaderboard/page.tsx
+++ app/menu/leaderboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { fetchLeaderboard } from "@/actions/server/use-server/fetch-leaderboard-server";
 import { useScoreStore } from "@/stores/useScoreStore";
+import { Loader2Icon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Leaderboard() {
@@ -23,14 +24,6 @@ export default function Leaderboard() {
     fetchData();
   }, []);
 
-  if (isLoading) {
-    return (
-      <div className="h-full w-full flex items-center justify-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-[#efcb68]"></div>
-      </div>
-    );
-  }
-
   return (
     <div className="space-y-4 flex flex-col">
       <div className="text-center ">
@@ -38,20 +31,26 @@ export default function Leaderboard() {
         <p className="text-sm">Top 50 players with highest score</p>
       </div>
       <div className="flex flex-col gap-2 text-sm flex-1 w-96 overflow-y-auto bg-black/5 border border-black/15 shadow-inner p-2 rounded-2xl z-50">
-        {leaderboard.map((player) => (
-          <div
-            key={player.id}
-            className="flex bg-white pl-2 py-2 pr-4 rounded-xl border border-black/20 shadow-sm items-center justify-between"
-          >
-            <div className="flex gap-2 items-center">
-              <div className="bg-[#efcb68] border border-black/15 shadow-sm aspect-square w-8 rounded-lg flex items-center justify-center">
-                {player.level}
+        {!isLoading ? (
+          leaderboard.map((player) => (
+            <div
+              key={player.id}
+              className="flex bg-white pl-2 py-2 pr-4 rounded-xl border border-black/20 shadow-sm items-center justify-between"
+            >
+              <div className="flex gap-2 items-center">
+                <div className="bg-[#efcb68] border border-black/15 shadow-sm aspect-square w-8 rounded-lg flex items-center justify-center">
+                  {player.level}
+                </div>
+                <p className="font-semibold">{player.username}</p>
               </div>
-              <p className="font-semibold">{player.username}</p>
+              <p className="">{player.highest_score}</p>
             </div>
-            <p className="">{player.highest_score}</p>
+          ))
+        ) : (
+          <div className="h-full w-full flex justify-center items-center">
+            <Loader2Icon className="animate-spin" />
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
--- components/left-side/game-info/components/player-level.tsx
+++ components/left-side/game-info/components/player-level.tsx
@@ -70,25 +70,25 @@ export default function PlayerLevel() {
           >
             <div className="flex justify-between items-center w-full bg-black/5 border border-black/15 shadow-inner p-2 rounded-xl">
               <div className="flex justify-between  items-center">
-                <div className="font-medium">{info.username}</div>
+                <div className="flex items-center gap-2">
+                  <div className="">
+                    <div
+                      onClick={handleSoundToggle}
+                      className={`${
+                        !isMuted ? "mainBgColor" : "bg-black/5"
+                      } h-8 w-8 flex justify-center items-center border border-black/15 shadow-sm rounded-lg`}
+                    >
+                      <Volume2Icon className="h-4 w-4" />
+                    </div>
+                  </div>
+
+                  <button className="  h-8 w-8 flex justify-center items-center border border-black/15 shadow-sm   rounded-lg">
+                    <ManageSubscription />
+                  </button>
+                </div>
               </div>
 
-              <div className="flex gap-2">
-                <div className="">
-                  <div
-                    onClick={handleSoundToggle}
-                    className={`${
-                      !isMuted ? "mainBgColor" : "bg-black/5"
-                    } h-8 w-8 flex justify-center items-center border border-black/15 shadow-sm rounded-lg`}
-                  >
-                    <Volume2Icon className="h-4 w-4" />
-                  </div>
-                </div>
-
-                <button className="  h-8 w-8 flex justify-center items-center border border-black/15 shadow-sm   rounded-lg">
-                  <ManageSubscription />
-                </button>
-
+              <div>
                 <button
                   onClick={() => signOut()}
                   className="bg-red-600 h-8 w-8 flex justify-center items-center border border-black/15 shadow-sm text-white  rounded-lg"
@@ -103,32 +103,32 @@ export default function PlayerLevel() {
         )}
       </AnimatePresence>
 
-      <div className="bg-black/5 col-span-10 w-full flex flex-col gap-2 relative overflow-hidden rounded-2xl p-2 border border-black/15 shadow-inner ">
+      <div className="bg-black/5 col-span-10 w-full flex flex-col gap-4 relative overflow-hidden rounded-2xl p-4 border border-black/15 shadow-inner ">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <div className="bg-[#efcb68] border border-black/15 shadow-sm z-20  font-medium  rounded-xl p-1 w-9 h-9 aspect-square flex justify-center items-center">
+            <div className="bg-[#efcb68] border border-black/15 shadow-sm z-20  font-medium  rounded-lg p-1 w-8 h-8 aspect-square flex justify-center items-center">
               {info.level}
             </div>
 
             <div className="z-10 font-medium leading-none text-center w-full">
-              <p className="font-medium">{info.username}</p>
+              <p className="font-medium text-[0.9rem]">{info.username}</p>
             </div>
           </div>
 
           <div
             onClick={() => setSetting(!setting)}
-            className="bg-white h-9 w-9 rounded-xl flex justify-center items-center border border-black/15 shadow-sm"
+            className="bg-white h-8 w-8 rounded-lg flex justify-center items-center border border-black/15 shadow-sm"
           >
             <SettingsIcon strokeWidth={1.7} className="h-5 w-5" />
           </div>
         </div>
 
-        <div className="bg-white h-6 overflow-hidden relative rounded-lg border border-black/15 shadow-sm">
+        <div className="bg-white h-8 overflow-hidden relative rounded-lg border border-black/15 shadow-sm">
           <p className="relative z-10 text-center text-xs flex items-center justify-center h-full">
             {info.xp} / 100
           </p>
           <motion.div
-            className="bg-[#efcb68] absolute top-0 left-0 flex justify-center items-center h-6 text-sm"
+            className="bg-[#efcb68] absolute top-0 left-0 flex justify-center items-center h-8 py-2 text-sm"
             animate={{ width: `${percentage}%` }}
             transition={{ duration: 0.5 }}
           ></motion.div>
