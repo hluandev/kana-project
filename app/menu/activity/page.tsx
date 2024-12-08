8b5313a38842a57046a6d4ef9119f5186c59c9b3
Ruan <97277916+ru-an-ng@users.noreply.github.com>
2024-12-08 00:08:14 -0800

    update

--- app/menu/activity/page.tsx
+++ app/menu/activity/page.tsx
@@ -33,8 +33,8 @@ export default function Activity() {
 
   return (
     <div className="overflow-hidden w-full">
-      <div className="flex flex-col space-y-4 rounded-2xl w-full h-full">
-        <div className="grid grid-cols-4 gap-4">
+      <div className="flex flex-col space-y-2 rounded-2xl w-full h-full">
+        <div className="grid grid-cols-4 gap-2">
           <ActivityBox
             loading={isLoading}
             title="Matches"
@@ -60,7 +60,7 @@ export default function Activity() {
           />
         </div>
 
-        <div className="flex h-full gap-4">
+        <div className="flex h-full gap-2">
           <GraphTable loading={isLoading} highestHandsTable={true} />
           <GraphTable loading={isLoading} highestHandsTable={false} />
         </div>
