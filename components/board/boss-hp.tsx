05005ce3e5d2ab5db3d5aca774173f8993db82a6
Ruan <97277916+ru-an-ng@users.noreply.github.com>
2024-12-13 03:25:06 -0800

    update

--- components/board/boss-hp.tsx
+++ components/board/boss-hp.tsx
@@ -1,12 +1,21 @@
 import { useScoreStore } from "@/stores/useScoreStore";
+import { motion } from "framer-motion";
 
 export const BossHp = () => {
   const bossHp = useScoreStore((state) => state.bossHp);
+  const percentage = Math.min((bossHp / 33900) * 100, 100);
   return (
-    <div className="bg-black/50 backdrop-blur-xl rounded-full  p-1 text-center">
-      <div className="bg-red-600 rounded-full p-1.5 font-medium text-base h-full w-full">
-        {bossHp}
-      </div>
+    <div className="bg-black/50 relative backdrop-blur-xl rounded-full  p-1 text-center">
+      <motion.div
+        initial={{ width: 0 }}
+        animate={{ width: `${percentage}%` }}
+        transition={{ duration: 0.5 }}
+        className="bg-red-600  rounded-full h-7 font-medium text-base  w-full"
+      >
+        <p className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-white">
+          {bossHp}
+        </p>
+      </motion.div>
     </div>
   );
 };
