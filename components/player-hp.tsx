fc01afb9f2da6e70e7c62bdca84a05b83d033b38
Ruan <97277916+ru-an-ng@users.noreply.github.com>
2024-12-13 22:52:30 -0800

    update

--- components/player-hp.tsx
+++ components/player-hp.tsx
@@ -17,12 +17,12 @@ export const PlayerHp = () => {
   return (
     <div className="flex flex-col">
       <div
-        className="gap-0.5 bg-red-600/30 w-44 backdrop-blur-xl p-0.5 grid rounded-full overflow-hidden"
+        className="gap-0.5 bg-red-600/30 w-fit backdrop-blur-xl p-0.5 grid rounded-full overflow-hidden"
         style={{ gridTemplateColumns: `repeat(${maxTurnsRef.current}, 1fr)` }}
       >
         {Array.from({ length: maxTurnsRef.current }).map((_, index) => (
           <div
-            className={`w-full h-3 rounded-full ${
+            className={` h-3 rounded-full w-8 ${
               index < turns ? "bg-red-600" : "bg-white/5"
             }`}
             key={index}
@@ -30,12 +30,12 @@ export const PlayerHp = () => {
         ))}
       </div>
       <div
-        className="gap-0.5 bg-blue-600/30 w-44 backdrop-blur-xl px-0.5 pb-0.5 grid rounded-full overflow-hidden"
+        className="gap-0.5 bg-blue-600/30 w-fit backdrop-blur-xl px-0.5 pb-0.5 grid rounded-full overflow-hidden"
         style={{ gridTemplateColumns: `repeat(${maxDiscardRef.current}, 1fr)` }}
       >
         {Array.from({ length: maxDiscardRef.current }).map((_, index) => (
           <div
-            className={`w-full h-3 rounded-full ${
+            className={`h-3 w-8 rounded-full ${
               index < discard ? "bg-blue-600" : "bg-white/5"
             }`}
             key={index}
