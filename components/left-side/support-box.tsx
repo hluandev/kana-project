5750d359469f431dbc1f7807c1f716c8e3ab6a3a
Ruan <97277916+ru-an-ng@users.noreply.github.com>
2024-12-17 00:23:01 -0800

    update

--- components/left-side/support-box.tsx
+++ components/left-side/support-box.tsx
@@ -6,8 +6,8 @@ interface SupportBoxProps {
 
 export const SupportBox = ({ text }: SupportBoxProps) => {
   return (
-    <div className="grid grid-cols-12 text-black/80">
-      <CheckIcon strokeWidth={1.5} className="w-4 pt-1 h-4 col-span-1" />{" "}
+    <div className="grid grid-cols-12 text-white/80">
+      <CheckIcon strokeWidth={1.5} className="w-4 pt-1 h-4 col-span-1" />
       <p className=" col-span-11 ">{text}</p>
     </div>
   );
--- components/left-side/support.tsx
+++ components/left-side/support.tsx
@@ -10,15 +10,13 @@ export const Support = () => {
   return (
     <>
       {!isSubscribed && (
-        <div
-          className={`text-black p-2 pb-3  w-full z-20 bg-[#efcb68] bg-opacity-40 border   rounded-xl`}
-        >
-          <div className="space-y-3">
+        <div className={`p-2 pb-3 bg-purple-600/30 w-full z-20 rounded-xl`}>
+          <div className="space-y-2">
             <SubscriptionButton />
 
             <div className="flex flex-col font-medium  gap-[10px]">
               <SupportBox text="Support this project." />
-              <SupportBox text="Gain early access to Stage 2 as soon as it's ready." />
+              <SupportBox text="Gain early access to Gate 3 (Kanji) as soon as it's ready." />
               <SupportBox text="Unlock Endless Mode." />
               <SupportBox text="Unlock the level cap at 10." />
             </div>
