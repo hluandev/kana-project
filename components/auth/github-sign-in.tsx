596c3923fb273c6f3d68b98f9a43f3d53825c335
Luan <97277916+hluandev@users.noreply.github.com>
2024-12-22 19:01:28 -0800

    update

--- components/auth/github-sign-in.tsx
+++ components/auth/github-sign-in.tsx
@@ -1,19 +1,19 @@
-"use client";
+// "use client";
 
-import { signInWithGithub } from "@/actions/server/google-login";
+// import { signInWithGithub } from "@/actions/server/google-login";
 
-export default function GithubSignIn() {
-  const handleSignIn = async () => {
-    try {
-      const { data } = await signInWithGithub();
-      // Supabase will handle the redirect automatically
-      if (data?.url) {
-        window.location.href = data.url;
-      }
-    } catch (error) {
-      console.error("Error signing in with Google:", error);
-    }
-  };
+// export default function GithubSignIn() {
+//   const handleSignIn = async () => {
+//     try {
+//       const { data } = await signInWithGithub();
+//       // Supabase will handle the redirect automatically
+//       if (data?.url) {
+//         window.location.href = data.url;
+//       }
+//     } catch (error) {
+//       console.error("Error signing in with Google:", error);
+//     }
+//   };
 
-  return <button onClick={handleSignIn}>GithubSignIn</button>;
-}
+//   return <button onClick={handleSignIn}>GithubSignIn</button>;
+// }
